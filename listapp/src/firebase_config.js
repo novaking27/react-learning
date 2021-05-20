e844e1d01ddc0dcbc5bde1f5561904a3b28395f9
novaking27 <elianderson@me.com>
2021-05-20 14:27:52 +1000

    Update firebase_config.js

--- listapp/src/firebase_config.js
+++ listapp/src/firebase_config.js
@@ -1,12 +1,12 @@
 import firebase from "firebase";
 
 var firebaseConfig = {
-    apiKey: "AIzaSyAJbuxb5AVckT1cAnyp1Hy5MmJ1Lfuehao",
-    authDomain: "todo-a52f7.firebaseapp.com",
-    projectId: "todo-a52f7",
-    storageBucket: "todo-a52f7.appspot.com",
-    messagingSenderId: "673376992928",
-    appId: "1:673376992928:web:fce27b38107f526a7213c5"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
 
   firebase.initializeApp(firebaseConfig);
